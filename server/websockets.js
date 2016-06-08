12ab134cb968c1787e20acc65fa570e9e708fbae
maxpleaner <maxpleaner@gmail.com>
2016-06-08 02:08:08 -0700

    make market move

--- server/websockets.js
+++ server/websockets.js
@@ -39,11 +39,13 @@ var WebsocketServer = module.exports = function(wsServer, server){
     var data = []
     var wsList = wsServer.connections
     for (var key of Object.keys(wsList)) {
-      var user = {}
-      user.latitude =  wsList[key].user.latitude
-      user.longitude =  wsList[key].user.longitude
-      user.username =  wsList[key].user.username
-      data.push(user)
+      if (wsList[key].user) {
+        var user = {}
+        user.latitude =  wsList[key].user.latitude
+        user.longitude =  wsList[key].user.longitude
+        user.username =  wsList[key].user.username
+        data.push(user)
+      }
     }
     return data
   }
@@ -60,27 +62,26 @@ var WebsocketServer = module.exports = function(wsServer, server){
   }
 
   function mapPing(openWs, user) {
+    user.latitude = user.latitude * Math.random()
+    user.longitude = user.longitude * Math.random()
     var wsList = wsServer.connections
     var sessionToken = user.sessionToken
     delete user.sessionToken
-    if (wsList[sessionToken].user) {cmd = "userChange"} else { cmd = "mapPing" }
+    if (wsList[sessionToken].user) {cmd = "userChange"} else { cmd = "mapData" }
     wsList[sessionToken].user = user
-    sendToAll({ cmd: "mapPing", user: user } )
+    sendToAll({ cmd: cmd, users: [user] } )
   }
 
   var onMessage = curry(function (openWs, message) {
     msg = JSON.parse(message)
     switch (msg.cmd) {
       case "login":
-        console.log("login")
         login(openWs, msg.sessionToken)
         break;
       case "loginConfirm":
-        console.log("loginConfirm")
         loginConfirm(openWs)
         break;
       case "mapPing":
-        console.log("mapPing")
         mapPing(openWs, msg.user)
         break;
     }
